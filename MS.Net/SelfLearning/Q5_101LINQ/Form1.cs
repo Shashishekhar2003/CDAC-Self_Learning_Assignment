90a6585990bf57a5441b5cdc26e6b0c2d1e14a01
Shashishekhar2003 <shashibache2003@gmail.com>
2026-07-29 16:35:48 +0530

    Added MS.Net Program

---
+++ MS.Net/SelfLearning/Q5_101LINQ/Form1.cs
@@ -0,0 +1,10 @@
+namespace Q5_101LINQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MS.Net/SelfLearning/Q6_LabExercise/Form1.cs
@@ -0,0 +1,10 @@
+namespace Q6_LabExercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
